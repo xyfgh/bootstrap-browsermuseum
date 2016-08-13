f2228ba3e90bfaf725407cbf3f3d31976940773a
xyfgh <953182870@qq.com>
2016-08-13 16:59:01 +0800

    更新

--- js/js.js
+++ js/js.js
@@ -1,12 +1,13 @@
-$(document).ready(function(){
+$(function(){
+	var i=0;
 	$("#xnavbar a").click(function(e){
 		var href = $(this).attr("href");
 		$("#xtabs a[href='" + href + "']").tab("show");
 		e.preventDefault();
 		$(document).scrollTop($(href).offset().top - 100);
-		// $("#xnavbarbtn").click();
 		if ($(window).width()<768) {
 			$("#xnavbarbtn").click();
 		}
+		console.log(++i);
 	});
 });
